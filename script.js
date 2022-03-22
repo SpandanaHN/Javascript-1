bde1ef88ac5575b2c9487541bbb262a131f72f9c
SpandanaHN <99424199+SpandanaHN@users.noreply.github.com>
2022-03-22 20:19:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+//Area of a Circle
+console.log("Area of a circle");
+let p=3.14
+console.log(p);
+var r= 5
+console.log(r);
+let area=(p*(r**2))
+console.log(area);
+
+
+//Circumference of a circle
+console.log("Circumference of a circle");
+var q=2
+console.log(q);
+let pi=3.14
+console.log(pi);
+var r=12
+console.log(r);
+const Circumference=q*pi*r
+console.log(Circumference);
+
+
+//Radius of a circle
+console.log("Radius of a circle");
+var c=4
+console.log(c);
+let PI=3.14
+console.log(PI);
+let a=2
+console.log(a);
+const Radius=c/PI*a
+console.log(Radius);
+
